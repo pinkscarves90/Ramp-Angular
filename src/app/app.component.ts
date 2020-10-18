25fda7bdb4e7b4576984d23423a6790025664db8
pinkscarves90 <pinkscarves90@gmail.com>
2020-10-18 17:34:45 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component, VERSION } from "@angular/core";
 import { RampInstantSDK } from "@ramp-network/ramp-instant-sdk";
+import { AppService } from "./app.service";
+import { takeUntil } from "rxjs/operators";
+import { Subject } from "rxjs";
 
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   name = "Durch.net";
-  constructor() {
+  users: any[] = [];
+  userCount = 0;
+
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  constructor(private appService: AppService) {
     new RampInstantSDK({
       hostAppName: "Durch.net",
       // URL to your app's logo
@@ -20,6 +28,15 @@ export class AppComponent {
       .on("*", event => console.log(event))
       .show();
   }
-
-  
+  public ngOnInit() {
+    //Do not remove as this is the idenetifier to check the http errors
+    this.appService
+      .addUser("")
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(data => {
+        console.log("message::::", data);
+        this.userCount = this.userCount + 1;
+        console.log(this.userCount);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { AppService } from "./app.service";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, HelloComponent],
+  providers: [AppService, HttpClient],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
