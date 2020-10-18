7aba18e0f8d823806e8d39a68c08c49a16ebd095
pinkscarves90 <pinkscarves90@gmail.com>
2020-10-19 00:24:13 +0530

    1 file modified

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,17 +1,17 @@
-import * as express from "express";
-import * as path from "path";
-import * as http from "http";
-import * as bodyParser from "body-parser";
+// // import * as express from "express";
+// // import * as path from "path";
+// // import * as http from "http";
+// // import * as bodyParser from "body-parser";
 
-class Server {
-  public app: express.Application;
+// class Server {
+//   public app: express.Application;
 
-  public static bootstrap(): Server {
-    return new Server();
-  }
+//   public static bootstrap(): Server {
+//     return new Server();
+//   }
 
-  constructor() {
-    // create expressjs application
-    this.app = express();
-  }
-}
+//   constructor() {
+//     // create expressjs application
+//     this.app = express();
+//   }
+// }
